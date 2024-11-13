1459e77fa6baa53f1309a09f2dd5f00a042a0b63
solveig.9@list.ru <solveig.9@list.ru>
2024-11-13 14:49:35 +0300

    change

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ const swiper = new Swiper(".swiper-container", {
     clickable: true,
   },
 });
+
+let swiperContainer = document.querySelector(".swiper-container");
+let swiperWrapper = document.querySelector(".swiper-wrapper");
+let swiperSlide = document.querySelectorAll(".swiper-slide");
+
+window.addEventListener("resize", function () {
+  let windowWidth = window.innerWidth;
+
+  if (windowWidth >= 768) {
+    swiperContainer.classList.remove("swiper-container");
+    swiperWrapper.classList.remove("swiper-wrapper");
+    swiperSlide.forEach(function (slide) {
+      slide.classList.remove("swiper-slide");
+    });
+  } else {
+    swiperContainer.classList.add("swiper-container");
+    swiperWrapper.classList.add("swiper-wrapper");
+    swiperSlide.forEach(function (slide) {
+      slide.classList.add("swiper-slide");
+    });
+  }
+});
